139be7271f9215bcca8d9dd338d9f17d5977f3b0
María Florencia Uruzagasti <muruzagasti87@gmail.com>
2022-10-15 08:54:50 -0300

    comentario innecesario

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { LocationProvider } from "./context/LocationContext";
 import { UserProvider } from './context/UserContext';
 
-/* const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "newLocation",
-    element: <NewLocation />,
-  }
-]); */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
